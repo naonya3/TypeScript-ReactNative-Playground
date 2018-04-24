96ba6b00760ee0226adec78679663a3842d81b0b
naonya3 <naoto.horiguchi@gmail.com>
2018-04-25 07:00:48 +0900

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import App from './build';
+
+AppRegistry.registerComponent('typescriptnative', () => App);
---
+++ src/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+const styles: any = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+});
+export default class App extends React.Component<Props, State> {
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <ScrollView>
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>
+                        Welcome to React Native
+                    </Text>
+                    <Text style={styles.instructions}>
+                        To get started, edit index.tsx
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Press Cmd+R to reload, {"\n"}
+                        Cmd+D or shake for dev menu!
+                    </Text>
+                </View>
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
+}
